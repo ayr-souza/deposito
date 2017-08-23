be8ae5d45d4806114af2a8e5ab05b289dda211e1
Trap Catcher <30628961+ayr-souza@users.noreply.github.com>
2017-08-23 10:36:36 -0300

    Add files via upload

---
+++ questaotupla/tupla01.py
@@ -0,0 +1,16 @@
+#!-*-coding: utf8-*-
+
+tupla = ('a', 'b', 'c')
+
+print(tupla)
+
+valor = input('Entre com a primeira letra')
+
+if valor in tupla:
+    print('A letra se encontra na tupla')
+else:
+    print('Não existe')
+
+
+
+
